1855cccc55a869dba5fb7a17f51d7373ae7661f9
Suo Qin <kpsesuo@gmail.com>
2015-08-24 22:07:01 +0800

    allow to search

--- src/kata.js
+++ src/kata.js
@@ -1,9 +1,15 @@
 function App(root) {
   var rootSelector = root;
+  function performSearch() {
+    var result = _.template("<ul class='result'></ul>");
+    $(result()).appendTo(rootSelector);
+  }
   return {
+    search: performSearch,
     index: function () {
-      var panel = _.template("<div class='panel'>panel</div>");
+      var panel = _.template("<div class='panel'>panel</div><input class='search-text' type='text'><input class='search' type='submit'>");
       $(panel()).appendTo(rootSelector);
+      $(rootSelector + " .search").on('click', performSearch())
     }
   };
 }
--- test/kata_spec.js
+++ test/kata_spec.js
@@ -7,4 +7,23 @@ describe("App", function () {
 
     expect(fixture.find('#app')).toContainElement('.panel')
   });
+
+  it("contains a search input", function () {
+    var fixture = setFixtures('<div id="app"></div>')
+    var app = new App('#app');
+
+    app.index();
+
+    expect(fixture.find('#app')).toContainElement('input.search')
+  });
+
+  it("should perform searching by clicking", function () {
+    var fixture = setFixtures('<div id="app"></div>')
+    var app = new App('#app');
+
+    app.index();
+    fixture.find('.search').click();
+
+    expect(fixture.find('#app')).toContainElement('ul.result')
+  });
 });
